fc64882b0dc8518cfd0141dcc1cfd5997085b6f5
HuyL <programa3324@gmail.com>
2024-11-15 10:55:01 +0000

    This is the battles

--- Battles.py
+++ Battles.py
@@ -1,5 +1,25 @@
 import random
 adventurer_sheet = {}
+def record_roll(creature_name, skill_score, stamina_score):
+    if creature_name not in adventurer_sheet
+        adventurer_sheet[creature_name] = {"Skill": skill_score, "Stamina": stamina_score}
 
-def record_roll(creature_name, skill_score, stamina_score)
-    if creature_name
+
+
+
+
+
+
+
+
+
+
+#
+# thisdict = {
+#     "brand": "Ford",
+#     "electric": False,
+#     "year": 1964,
+#     "colors": ["red", "white", "blue"]
+#         }
+# thisdict.update({"colors" : "red"})
+# print(thisdict)
